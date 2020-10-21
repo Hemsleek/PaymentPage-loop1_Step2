0445e5779eb7803e6df79a49d61fba07435dac11
hemsleek <asiyanbimubashir@gmail.com>
2020-10-21 21:12:20 +0100

    fix: indicator style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "my-blue": "#598BDD"
+                "my-blue": "#598BDD",
+                "faint-grey": "#EFEFEF",
+                'active-brown': '#A3A3A3',
+                'faint-brown': '#D5D5D5'
             }
         },
     },
